e359d8a05042112c760689cef88222480b774bca
Administrator <Administrator@USER-20170323BR>
2020-08-04 08:58:19 +0800

    test

--- src/com/how2java/ExceptionJob1.java
+++ src/com/how2java/ExceptionJob1.java
@@ -12,7 +12,7 @@ public class ExceptionJob1 implements Job{
 		try {
 			System.out.println(100/i);
 		} catch (Exception e) {
-			System.out.println("发生了异常，取消这个job对应的所有调度111111111111111111111111111111111111111");
+			System.out.println("发生了异常，取消这个job对应的所有调度11111111111111112222111111");
 			JobExecutionException je=new JobExecutionException(e);
 			je.setUnscheduleAllTriggers(true);
 			throw je;
